3b1c05924b656b9764fdfc034e2f4bcbb90ea39e
Artem Brilev <razdvajonez@mail.ru>
2020-05-20 00:40:48 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -4,76 +4,83 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import WebSocket from 'ws';
 import bodyParser from 'body-parser';
+import { createServer } from 'http';
 import { v4 } from 'uuid';
 
 const PORT = process.env.PORT || 4000;
-const WS_PORT = process.env.WS_PORT ? +process.env.WS_PORT : 4001;
-
+const server = createServer();
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 
-const wss = new WebSocket.Server({ port: WS_PORT });
+const wss = new WebSocket.Server({ server });
 
+server.on('request', app);
 
 interface IData {
     offer: any;
+    candidate: any;
+    answer: any;
     id: string;
     userId: string;
+    type: string;
 }
 
-const map = new Map<string, Pick<IData, 'offer' | 'userId'>[]>();
+const map = new Map<string, Pick<IData, 'offer' | 'userId'>>();
 const users = new Map<string, WebSocket>();
 const sockets = new Map<WebSocket, string>();
-
-const add = <T>(map: Map<any, T[]>, id: string | WebSocket, data: T) => {
-    if (map.has(id)) {
-        const arr = map.get(id)!;
-        arr.push(data);
-    } else {
-        map.set(id, [data]);
-    }
-};
+const candidates = new Map<string, any[]>();
 
 wss.on('connection', (ws, req) => {
     const { url } = req;
     if (!url) {
         return;
     }
-    const [, id] = url.split('=');
+    const [, currentUserId] = url.split('=');
 
     ws.on('message', (message) => {
-        const { id, userId, offer } = JSON.parse(message.toString()) as IData;
-        add(map, id, { offer, userId });
-        users.set(userId, ws);
-        sockets.set(ws, userId);
-        const data = map.get(id)!;
-        data.forEach(user => {
-            if (userId === user.userId) {
+        const { id: channelId, userId: reqUserId, offer, type, candidate, answer } = JSON.parse(message.toString()) as IData;
+        if (type === 'offer') {
+            const user = map.get(channelId);
+            if (user) {
+                const socket = users.get(user.userId);
+                if (socket) {
+                    socket.send(JSON.stringify({ message: 'offer', data: { offer, id: channelId, userId: reqUserId } }));
+                }
+            }
+
+            map.set(channelId, { offer, userId: reqUserId });
+            users.set(reqUserId, ws);
+            sockets.set(ws, reqUserId);
+        }
+
+        if (type === 'answer') {
+            const data = users.get(reqUserId);
+            if (!data) {
                 return;
             }
+            data.send(JSON.stringify({ message: 'answer', data: answer }));
+            data.send(JSON.stringify({ message: 'candidates', data: candidates.get(currentUserId) }));
+        }
 
-            const socket = users.get(user.userId);
-            if (!socket) {
-                return
+        if (type === 'candidate') {
+            const arr = candidates.get(reqUserId);
+            if (arr) {
+                arr.push(candidate);
+            } else {
+                candidates.set(reqUserId, [candidate]);
             }
-
-            socket.send(JSON.stringify({ message: 'offer', data:  { offer, id, userId } }));
-        });
+        }
     });
 
     ws.on('close', () => {
-        const userId = sockets.get(ws)!;
-        users.delete(userId);
-        const data = map.get(id);
-        if (!data) {
-            return;
-        }
-        const arr = data.filter(o => o.userId !== userId);
-        arr.length ? map.set(id, arr) : map.delete(id);
+        users.delete(currentUserId);
+        sockets.delete(ws);
+        candidates.delete(currentUserId);
     });
 });
 
@@ -82,15 +89,6 @@ app.post('/channel', (req, response) => {
     response.send(id);
 });
 
-app.post('/answer', ({body: { userId, answer}}, response) => {
-    console.log('answer', userId, answer);
-    const data = users.get(userId);
-    if (data) {
-        data.send(JSON.stringify({ message: 'answer', data: answer}));
-    }
-    response.send();
-});
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
